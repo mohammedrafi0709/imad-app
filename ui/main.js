070e0da8c0bfb3172e0f099fe9c0c8e5ea4f53fb
mohammedrafi0709 <30713576+mohammedrafi0709@users.noreply.github.com>
2017-08-15 08:06:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!')
 var img = document.getelementbyID ('IMG');
 IMG.ONCLICK = FUNCTION (); {
+    img.style.marginleft = '100px';
     var interval = setInterval (moveRight, 100);
 }
