d6a96cbfadfb42c4d16b2734662dfa7835bb7a3c
mohammedrafi0709 <30713576+mohammedrafi0709@users.noreply.github.com>
2017-08-17 09:04:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,17 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function (){
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-  //var request = new XMLHttpRequest();
-  //request.onreadystatechange = function (){
-    //  if (request.stutus === 200 ){
-      //    var counter = request.responseText;
-        //  var span = document.getElementById('count')
-      //}
- // }
+
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function (){
+      if (request.readyState === XMLHttprequest.DONE){
+    if (request.status === 200 ){
+      var counter = request.responseText;
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
+      }
+  }
+};
+request.open('GET', 'http://http://mohammedrafi0709.imad.hasura-app.io/', true);
+request.send(null);
 };
