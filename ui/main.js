ee6358c6d8cedec98ad6c50e0dd9177a48c7ef3a
mohammedrafi0709 <30713576+mohammedrafi0709@users.noreply.github.com>
2017-08-17 08:52:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,14 @@
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function (){
   counter = counter + 1;
-  var span = document.getElementById("count");
+  var span = document.getElementById('count');
   span.innerHTML = counter.toString();
+  //var request = new XMLHttpRequest();
+  //request.onreadystatechange = function (){
+    //  if (request.stutus === 200 ){
+      //    var counter = request.responseText;
+        //  var span = document.getElementById('count')
+      }
+  }
 };
