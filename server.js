99f11a9bceddd894c35cffcfdf138200439f3b7b
mohammedrafi0709 <30713576+mohammedrafi0709@users.noreply.github.com>
2017-08-26 09:25:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config= {
     database : 'mohammedrafi0709',
     host : 'db.imad.hasura-app.io',
     port: '5432',
-    password : process.emv.DB_PASSWORD
+    password : process.env.DB_PASSWORD
 };
 
 var app = express();
@@ -21,6 +21,7 @@ app.get('/counter', function (req, res){
 var pool = new pool(config);
 
 app.get('test/db', function (req, res){
+    
     pool.query('SELECT * FROM test', function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
