d69ad5b41e1fc113e2f57408b898e51c7dc00cd2
mohammedrafi0709 <30713576+mohammedrafi0709@users.noreply.github.com>
2017-08-28 10:52:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var app = express();
+app.use(morgan('combined'));
 var Pool = require('pg').Pool;
 var config= {
     user : 'mohammedrafi0709' ,
@@ -10,8 +12,7 @@ var config= {
     password : process.env.DB_PASSWORD
 };
 
-var app = express();
-app.use(morgan('combined'));
+
 
 var pool = new Pool(config);
 
